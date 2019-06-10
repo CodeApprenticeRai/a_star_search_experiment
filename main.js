194a5718e3a55002b0f376b15a3c88a14bddefd4
Tare <tare.dgg@gmail.com>
2019-06-10 17:40:06 -0400

    several objectives completed

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ function init(canvas, ctx) {
   var grid_margin = 0;
   row_step = canvas.height / num_rows;
   column_step = canvas.width / num_columns;
-  // draw column lines
+
+  // draw gridlines
   for ( var i=0; i < canvas.width; i += this.column_step){
     ctx.beginPath();
     ctx.moveTo(i, grid_margin);
     ctx.lineTo(i, canvas.height-grid_margin);
     ctx.stroke();
   }
-
   for ( var j=0; j < canvas.height; j += this.row_step){
     ctx.beginPath();
     ctx.moveTo(grid_margin, j);
@@ -25,8 +25,31 @@ function init(canvas, ctx) {
     ctx.stroke();
   }
 
-  // iterate squres, do some conditional logic on to format each based on the board state
-
+  // it erate squres, do some conditional logic on to format each based on the board state
+  // for ( var i=0; i < grid.board_state.length; i++){
+  //   for (var j=0; j < grid.board_state[i].length; j++){
+  //     sqaure_state = grid.board_state[i][j];
+  //     if (square_state == 1){
+  //       ctx.fillStyle = 'green';
+  //       ctx.fillRect( i*column_step, j*row_step,column_step, row_step);
+  //     }
+  //     else if (square_state == 2){
+  //       ctx.fillStyle = 'grey';
+  //       ctx.fillRect( i*column_step, j*row_step,column_step, row_step);
+  //     }
+  //     else if (square_state == 3){
+  //       ctx.fillStyle = 'red';
+  //       ctx.fillRect( i*column_step, j*row_step,column_step, row_step);
+  //     }
+  //   }
+  // }
+  // ctx.fillStyle = 'grey';
+  // ctx.fillRect( 5*column_step, 5*row_step,column_step, row_step);
+  ctx.beginPath();
+  ctx.fillStyle = 'black';
+  ctx.arc( 5*column_step + column_step/2, 5*row_step + row_step/2, column_step-30, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.stroke();
 }
 
 
@@ -38,7 +61,10 @@ class Grid {
     this.board_state = new Array(num_rows);
 
     for (var i=0; i < num_rows; i++){
-       this.board_state[i] = new Array(num_columns);
+      this.board_state[i] = new Array(num_columns);
+      for (var j=0; j < num_columns; j++){
+          this.board_state[i][j] = 0;
+      }
     }
   }
 }
@@ -48,19 +74,15 @@ class BoardObject{
 }
 
 class FailureSquare extends BoardObject {
-
 }
 
 class GoalSquare extends BoardObject {
-
 }
 
 class BlockedSqaure extends BoardObject {
-
 }
 
 class Agent extends BoardObject{
-
 }
 
 
